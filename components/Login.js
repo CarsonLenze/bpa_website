d26912174186c26f5b628bed8cedf70576851b84
CarsonLenze <carsonlenze@gmail.com>
2022-10-19 21:57:18 -0400

    commit

--- components/Login.js
+++ components/Login.js
@@ -1,80 +1,81 @@
 import { Modal, Button, Text, Input, Row, Checkbox, Loading } from "@nextui-org/react";
 import { Password, Mail } from "./Icons";
-import { useState, useRef } from "react";
+import { useForm } from 'react-hook-form';
+import { useState,useCallback } from 'react';
 
 export default function Login({ state, close }) {
     const [loading, setLoading] = useState(false);
     const [errors, setErrors] = useState({ });
     const closeHandler = () => close(false);
-    const password = useRef(null);
-    const email = useRef(null);
 
-    const submit = () => {
+    const { register, reset, handleSubmit } = useForm();
+
+    const onSubmit = useCallback(async (data) => {
+        //console.log('form data:', data);
+        //reset()
         const error = {}
-        if (!email.current.value) error = { ...error, Email: 'Please enter a Email' }
-        if (!password.current.value) error = { ...error, Password: 'Please enter a Password' }
+        if (!data.email) error = { ...error, Email: 'Please enter a Email' }
+        if (!data.password) error = { ...error, Password: 'Please enter a Password' }
         if (Object.keys(error)[0]) return setErrors(error);
-        console.log({ email: email.current.value, password: password.current.value})
+        console.log(data)
         setLoading(true);
-    }
+    }, []);
 
     return (
         <div>
             <Modal closeButton blur open={state} onClose={closeHandler}>
-                <Modal.Header>
-                    <Text size={18}>
-                        {'Welcome to '}
-                        <Text b size={18}>
-                            TBD
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Modal.Header>
+                        <Text size={18}>
+                            {'Welcome to '}
+                            <Text b size={18}>
+                                TBD
+                            </Text>
                         </Text>
-                    </Text>
-                </Modal.Header>
-                <Modal.Body>
-                    <Input
-                        bordered
-                        fullWidth
-                        color="primary"
-                        size="lg"
-                        placeholder="Email"
-                        id="email"
-                        ref={email}
-                        disabled={loading}
-                        helperColor="error"
-                        helperText={errors.Email}
-                        onChange={() => { if (errors) setErrors({}) }}
-                        contentLeft={<Mail fill="currentColor" />}
-                    />
-                    <Input.Password
-                        bordered
-                        fullWidth
-                        color="primary"
-                        size="lg"
-                        id="password"
-                        placeholder="Password"
-                        ref={password}
-                        disabled={loading}
-                        helperColor="error"
-                        helperText={errors.Password}
-                        onChange={() => { if (errors) setErrors({}) }}
-                        contentLeft={<Password fill="currentColor" />}
-                    />
-                    <Row justify="space-between">
-                        <Checkbox defaultSelected={true}>
-                            <Text size={14}>Remember me</Text>
-                        </Checkbox>
-                        <Text size={14}>Forgot password?</Text>
-                    </Row>
-                </Modal.Body>
-                <Modal.Footer justify="space-between">
-                    <Button auto>
-                        Sign up
-                    </Button>
-                    {loading ? <Button auto disabled>
-                        <Loading color="currentColor" size="sm" />
-                    </Button> : <Button auto onClick={submit}>
-                        Sign in
-                    </Button>}
-                </Modal.Footer>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Input
+                            bordered
+                            fullWidth
+                            color="primary"
+                            size="lg"
+                            placeholder="Email"
+                            {...register('email')}
+                            helperColor="error"
+                            helperText={errors.Email}
+                            onChange={() => { if (errors) setErrors({}) }}
+                            contentLeft={<Mail fill="currentColor" />}
+                        />
+                        <Input.Password
+                            bordered
+                            fullWidth
+                            color="primary"
+                            size="lg"
+                            placeholder="Password"
+                            {...register('password')}
+                            helperColor="error"
+                            helperText={errors.Password}
+                            onChange={() => { if (errors) setErrors({}) }}
+                            contentLeft={<Password fill="currentColor" />}
+                        />
+                        <Row justify="space-between">
+                            <Checkbox defaultSelected={true}>
+                                <Text size={14}>Remember me</Text>
+                            </Checkbox>
+                            <Text size={14}>Forgot password?</Text>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer justify="space-between">
+                        <Button auto>
+                            Sign up
+                        </Button>
+                        {loading ? <Button auto disabled>
+                            <Loading color="currentColor" size="sm" />
+                        </Button> : <Button auto type="submit">
+                            Sign in
+                        </Button>}
+                    </Modal.Footer>
+                </form>
             </Modal>
         </div>
     )
